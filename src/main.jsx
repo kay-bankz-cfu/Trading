98cdf9544ce37fb906067e9889d3ee0cc8c56de4
git <abdulrahmanbadmus240@gmail.com>
2026-01-02 17:37:44 +0100

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 // import { StrictMode } from 'react'
 // import { createRoot } from 'react-dom/client'
@@ -9,8 +10,8 @@ import App from "./App.jsx";
 
 ReactDom.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
