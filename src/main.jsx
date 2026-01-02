d7c8878fea0ebc96e4053f7296b3dc61a8fa4fac
git <abdulrahmanbadmus240@gmail.com>
2026-01-02 23:34:40 +0100

    Fix base path for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,11 @@
 import React from "react";
-import ReactDom from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-import "./index.css";
-import App from "./App.jsx";
-
-ReactDom.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Trading">
       <App />
     </BrowserRouter>
   </React.StrictMode>
